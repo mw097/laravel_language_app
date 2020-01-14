c692e931f3fd7b60fdb32c0db918746dfdb5fc63
Adrian Ochab <ochabadrian1@gmail.com>
2020-01-14 19:09:36 +0100

    Add TranslateWord and OrderSentence Quiz functionality

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -16,8 +16,4 @@ class QuizzesController extends Controller
         return view('quizzes.create');
     }
 
-    public function translate_words()
-    {
-        return view('quizzes.type.translate_words');
-    }
 }
