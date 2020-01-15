5e50cc613829dfa044671a2f3dcb908f603ceec6
mw097 <mihelone@gmail.com>
2020-01-15 18:03:05 +0100

    Add Codeception and first test

---
+++ tests/acceptance/mainPageCest.php
@@ -0,0 +1,37 @@
+<?php
+
+class mainPageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('have main page');
+
+        $I->amOnPage('/');
+        $I->see('LanguageApp');
+        $I->see('Login');
+        $I->see('Register');
+
+        $I->amGoingTo('Login');
+        $I->click('Login');
+        $I->fillField('email', 'john.doe@gmail.com');
+        $I->fillField('password', 'secret123');
+        $I->click('#login_button');
+
+        $I->canSeeCurrentUrlEquals('/home');
+        $I->see('john');
+        $I->see('Dashboard');
+
+        $I->amOnPage('/');
+        $I->see('LanguageApp');
+        $I->see('Home');
+        $I->see('Create quiz');
+        $I->see('Quiz list');
+        $I->see('Add language');
+        $I->see('About us');
+    }
+}
---
+++ tests_codeception/_data/dump.sql
@@ -0,0 +1,326 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.19.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `choose_pictures`
+--
+
+DROP TABLE IF EXISTS `choose_pictures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choose_pictures` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choose_pictures`
+--
+
+LOCK TABLES `choose_pictures` WRITE;
+/*!40000 ALTER TABLE `choose_pictures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choose_pictures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `choose_translations`
+--
+
+DROP TABLE IF EXISTS `choose_translations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choose_translations` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `native` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foreign_correct` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foreign_1` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foreign_2` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foreign_3` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choose_translations`
+--
+
+LOCK TABLES `choose_translations` WRITE;
+/*!40000 ALTER TABLE `choose_translations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choose_translations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `failed_jobs`
+--
+
+LOCK TABLES `failed_jobs` WRITE;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `l_t_s`
+--
+
+DROP TABLE IF EXISTS `l_t_s`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `l_t_s` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `language_id` bigint(20) unsigned NOT NULL,
+  `translate_sentence_id` bigint(20) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `l_t_s_language_id_translate_sentence_id_unique` (`language_id`,`translate_sentence_id`),
+  KEY `l_t_s_translate_sentence_id_foreign` (`translate_sentence_id`),
+  CONSTRAINT `l_t_s_language_id_foreign` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `l_t_s_translate_sentence_id_foreign` FOREIGN KEY (`translate_sentence_id`) REFERENCES `translate_sentences` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `l_t_s`
+--
+
+LOCK TABLES `l_t_s` WRITE;
+/*!40000 ALTER TABLE `l_t_s` DISABLE KEYS */;
+INSERT INTO `l_t_s` VALUES (1,1,2,NULL,NULL),(2,1,1,NULL,NULL),(3,2,1,NULL,NULL),(4,2,2,NULL,NULL);
+/*!40000 ALTER TABLE `l_t_s` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `languages`
+--
+
+DROP TABLE IF EXISTS `languages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `languages` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `language` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `languages`
+--
+
+LOCK TABLES `languages` WRITE;
+/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
+INSERT INTO `languages` VALUES (1,'Angielski','2020-01-15 10:54:45','2020-01-15 10:54:45'),(2,'SQL','2020-01-15 10:54:47','2020-01-15 10:54:47');
+/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2020_01_09_194136_create_translate_words_table',1),(5,'2020_01_09_194242_create_translate_sentences_table',1),(6,'2020_01_09_194321_create_choose_translations_table',1),(7,'2020_01_09_194332_create_choose_pictures_table',1),(8,'2020_01_09_194348_create_order_sentences_table',1),(9,'2020_01_11_194251_create_languages_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_sentences`
+--
+
+DROP TABLE IF EXISTS `order_sentences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_sentences` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `sentence` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_sentences`
+--
+
+LOCK TABLES `order_sentences` WRITE;
+/*!40000 ALTER TABLE `order_sentences` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_sentences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `translate_sentences`
+--
+
+DROP TABLE IF EXISTS `translate_sentences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `translate_sentences` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `foreign` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `native` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `translate_sentences`
+--
+
+LOCK TABLES `translate_sentences` WRITE;
+/*!40000 ALTER TABLE `translate_sentences` DISABLE KEYS */;
+INSERT INTO `translate_sentences` VALUES (1,'qwe','qwe','Angielski','2020-01-15 10:54:54','2020-01-15 10:54:54'),(2,'qweqwewqe','qweqwe','SQL','2020-01-15 10:55:03','2020-01-15 10:55:03');
+/*!40000 ALTER TABLE `translate_sentences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `translate_words`
+--
+
+DROP TABLE IF EXISTS `translate_words`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `translate_words` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `foreign` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `native` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `translate_words`
+--
+
+LOCK TABLES `translate_words` WRITE;
+/*!40000 ALTER TABLE `translate_words` DISABLE KEYS */;
+/*!40000 ALTER TABLE `translate_words` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'john','john.doe@gmail.com',NULL,'$2y$10$thw9DubAecCqTZJNSIw1gukRkEyfDkE5SqXN1F4YcAf0nzSXebGbu',NULL,'2020-01-15 10:54:40','2020-01-15 10:54:40');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-15 16:44:28
