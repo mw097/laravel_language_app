df629ea18763bd02b3ba2777e5159e4526c5e155
mw097 <mihelone@gmail.com>
2020-01-15 13:07:08 +0100

    ERROR

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Language;
+use App\TranslateSentence;
 use Illuminate\Http\Request;
 
 class QuizzesController extends Controller
 {
     public function index()
     {
-        return view('quizzes.show');
+        if(request('language'))
+        {
+            $test = Language::where('language', request('language'))->firstOrFail()->translate_sentences;
+            dd($test);
+            return view('quizzes.show', [
+                'languages' => Language::all(),
+                //'translate_sentences' => Language::where('language', request('language'))->firstOrFail()->translate_sentences
+                'translate_sentences' => TranslateSentence::latest()->get()
+            ]);
+        }else{
+            return view('quizzes.show', [
+                'languages' => Language::all(),
+                'translate_sentences' => TranslateSentence::latest()->get()
+            ]);
+        }
+//        return view('quizzes.show',[
+//            'languages' => Language::all()
+//        ]);
     }
 
     public function create()
--- resources/views/quizzes/show.blade.php
+++ resources/views/quizzes/show.blade.php
@@ -1,75 +1,24 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>List Quiz</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-</head>
-<body>
+@section('content')
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
             List Quiz
         </div>
+        <form>
+            Select language:
+            <select name="quizType" onchange="if (this.value) window.location.href='/quiz'+this.value">
+                @foreach($languages as $language)
+                    <option value="?language={{$language->language}}"{{Request::segment(2) === $language->language ? 'selected' : ''}}>{{$language->language}}</option>
+                @endforeach
+            </select>
+        </form>
     </div>
+    @foreach ($translate_sentences as $ts)
+       <ul>
+           <li>{{$ts}} : {{$ts}}</li>
+       </ul>
+    @endforeach
 </div>
-</body>
-</html>
+@endsection
