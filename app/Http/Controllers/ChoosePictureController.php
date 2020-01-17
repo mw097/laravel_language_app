09980ae37b7db9caf9036caae654490763fbc649
Student <szymsajka@gmail.com>
2020-01-17 18:43:45 +0100

    Delete unnecesarry comments

--- app/Http/Controllers/ChoosePictureController.php
+++ app/Http/Controllers/ChoosePictureController.php
@@ -35,14 +35,9 @@ class ChoosePictureController extends Controller
 
         $quiz = new ChoosePicture();
         $quiz->foreign= $request->foreign;
-        //$quiz->image_correct = $request->image_correct;
-        //$quiz->image_1 = $request->image_1;
-        //$quiz->image_2 = $request->image_2;
-        //$quiz->image_3 = $request->image_3;
         $quiz->language = $request->language;
 
         $file = $request->image_correct;
-        //$extension = $file->getClientOriginalExtension();
         $filename = $file->getClientOriginalName();
         $file -> move('uploads/images/', $filename);
         $quiz->image_correct = $filename;
@@ -67,16 +62,4 @@ class ChoosePictureController extends Controller
 
         return redirect('/quiz');
     }
-
-   /* public function validateChoosePicture()
-    {
-        return request()->validate([
-            'foreign' => 'required|max:200',
-            'image_correct' => 'required|image|dimensions:min_width=100, min_height=200',
-            'image_1' => 'required|image|dimensions:min_width=100, min_height=200',
-            'image_2' => 'required|image|dimensions:min_width=100, min_height=200',
-            'image_3' => 'required|image|dimensions:min_width=100, min_height=200',
-            'language' => 'required'
-        ]);
-    }*/
 }
