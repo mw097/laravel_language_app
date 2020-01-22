43ec62f3f3ef268b2bcb19133954ca4b0c768149
mw097 <mihelone@gmail.com>
2020-01-22 01:05:27 +0100

    Clean up

--- resources/views/quizzes/show.blade.php
+++ resources/views/quizzes/show.blade.php
@@ -6,15 +6,6 @@
         <div class="title m-b-md">
             List Quiz
         </div>
-{{--        <form>--}}
-{{--            Select language:--}}
-{{--            <select name="quizType" onchange="if (this.value) window.location.href='/quiz'+this.value">--}}
-{{--                <option>Select language...</option>--}}
-{{--                @foreach($languages as $language)--}}
-{{--                    <option value="?language={{$language->language}}"{{Request::segment(2) === $language->language ? 'selected' : ''}}>{{$language->language}}</option>--}}
-{{--                @endforeach--}}
-{{--            </select>--}}
-{{--        </form>--}}
     </div>
     <a href="{{ route('translateWords.index') }}">Translate words </a> <br>
     <a href="{{ route('translateSentences.index') }}">Translate sentences </a> <br>
